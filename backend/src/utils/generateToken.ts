ed163c52166bbc10b3aac75965bc74b858b98dea
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-06-27 23:35:09 +0530

    fix: cookie secure in production env only

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -1,6 +1,6 @@
 import { Response } from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "../config/server.config";
+import { JWT_SECRET, NODE_ENV } from "../config/server.config";
 
 export const generateToken = (userId: String, res: Response) => {
   const token = jwt.sign({ userId }, JWT_SECRET!, {
@@ -9,7 +9,7 @@ export const generateToken = (userId: String, res: Response) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: true,
+    secure: NODE_ENV === "production",
     sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
