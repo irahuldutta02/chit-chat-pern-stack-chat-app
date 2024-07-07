0b4334c36f086673a5954b924a4bb58632b8238e
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-07-07 12:32:39 +0530

    chore: Add cors package for handling cross-origin resource sharing (CORS)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,14 +3,15 @@ import { PORT } from "./config/server.config";
 import authRouter from "./routes/auth.routes";
 import messageRouter from "./routes/message.routes";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.text());
-
 app.use(cookieParser());
+app.use(cors());
 
 app.get("/", (req, res) => {
   return res.status(200).json({
